4ab47a0cb019994bf8fc8b268e38754e2a41743f
Busari <muhammedbusari123@gmail.com>
2022-10-18 18:27:39 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table -prints tables
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = a * b;
+
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '8');
+		}
+		_putchar('\n');
+	}
+}
