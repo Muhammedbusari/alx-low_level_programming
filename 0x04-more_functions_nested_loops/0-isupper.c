19e032e6c9a5678a4d3ef006b36fca337af8fe81
Busari <muhammedbusari123@gmail.com>
2022-10-20 17:36:50 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,14 @@
  * Return: 1 if c is uppercase or 0 otherwise
  */
 int _isupper(int c)
+
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
-		return (0);
+	{
+		return(0);
+	}
 }
