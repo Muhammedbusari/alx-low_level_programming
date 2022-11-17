fa10423889868986401a7bfe7e7b7d0d7c5bd0d0
Busari <muhammedbusari123@gmail.com>
2022-11-17 04:16:06 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-/**
+/*
  * File: 0-sum_them_all.c
  * Auth: Busari Muhammed
  */
@@ -7,9 +7,9 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * sum_them_all - Returns the sum of all its paramters
+ * @n: The number of paramters passed to the function
+ * @...: A variable number of paramters to calculate the sum of
  * Return: If n == 0 - 0
  * Otherwise - the sum of all parameters
  */
